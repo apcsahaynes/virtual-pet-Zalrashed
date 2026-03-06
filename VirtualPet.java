c8eef7e417969689154036dcd7ee3a9a25fc7d27
Zalrashed <zain.alrashed@jpschools.org>
2026-03-06 10:59:04 -0600

    Update VirtualPet.java

--- VirtualPet.java
+++ VirtualPet.java
@@ -1,4 +1,4 @@
-public class VirtualPet4 {
+public class VirtualPet {
   private String name;
   private int energy;
   private int weight;
@@ -7,7 +7,7 @@ public class VirtualPet4 {
   private int ageYears;
   
 
-  public VirtualPet4(String name){
+  public VirtualPet(String name){
     this.name = name;
     this.energy = 0;
     this.weight = 5;
@@ -27,21 +27,28 @@ public class VirtualPet4 {
   public int getHappinessLevel(){
     return happiness;
   }
-
+  public int getWeight(){
+  return weight;
+  }
+  public int getAgeYears(){
+  return ageYears;
+  }
   public void feed(Food food){
    this.energy += food.getEnergyIncrease();
    this.weight += food.getWeightGain();
    this.happiness += food.getHappinessIncrease();
-   
+   this.weight = Math.max(Math.min(this.weight, 100), 5);
+   this.energy = Math.max(Math.min(this.energy, 25), 0);
+   this.happiness = Math.max(Math.min(this.happiness, 100), 0);
     
   }
 
   public void play(Game game){
   if (game.isWinner()) { 
    this.happiness += game.getHappinessIncr();
-   this.happiness = Math.max(Math.min(this.happiness, 10), 0);
+   this.happiness = Math.max(Math.min(this.happiness, 100), 0);
    this.weight -= game.getWeightDecr();
-   this.weight = Math.max(this.weight, 5); 
+   this.weight = Math.max(Math.min(this.weight, 100), 5);
   }
   }
 
@@ -62,7 +69,12 @@ public class VirtualPet4 {
       ageMonths = 0;
     }
   } 
-
+  public void beatIt(Beat beat){
+   if (getWeight() > 10){
+     this.weight -= beat.getWeightDecr();
+     this.happiness -= beat.getHappinessDecr();
+   }
+  }
   public String toString(){
     return name + "'s information: \nEnergy: " + energy 
     + "\nWeight: " + weight + "\nHappiness: " + happiness
